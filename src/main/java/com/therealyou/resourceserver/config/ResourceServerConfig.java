61a7f1beff357fa668ccd766d83e77b828fdf216
xev1l <xev1l1th@gmail.com>
2021-01-14 00:19:50 +0500

    resource server with keyloack as an auth server

--- src/main/java/com/therealyou/resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/therealyou/resourceserver/config/ResourceServerConfig.java
@@ -1,31 +1,25 @@
 package com.therealyou.resourceserver.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
-
-    @Value(value = "${jwt.secret.key}")
-    private String secret;
-
-
-    @Bean
-    public JwtAccessTokenConverter converter(){
-        var converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(secret);
-        return converter;
-    }
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Bean
     public TokenStore tokenStore(){
-        return new JwtTokenStore(converter());
+        return new JwkTokenStore("http://localhost:8080/auth/realms/master/protocol/openid-connect/certs");
     }
 
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources){
+        resources.tokenStore(tokenStore())
+                .resourceId("xev1l_client");
+    }
 }
--- src/main/java/com/therealyou/resourceserver/controller/HelloController.java
+++ src/main/java/com/therealyou/resourceserver/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.therealyou.resourceserver.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +10,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
-    public ResponseEntity<Map<String, Object>> hello(){
+    public ResponseEntity<Map<String, Object>> hello(Authentication authentication){
         Map<String, Object> map = new HashMap<>();
         map.put("roflan", "ebalo");
         map.put("pog", "champ");
+        log.info(authentication.toString());
         return ResponseEntity.status(200).body(map);
     }
 }
